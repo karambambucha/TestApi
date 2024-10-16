ba98fd1f84a5c4e84e96e164435d0d19cdb7d47c
karambambucha <tsmfdr@yandex.ru>
2024-10-17 00:02:18 +0700

    add doctorController.getAppoinments

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -2,31 +2,60 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 class doctorController {
-    async getAppointments(req, res, next) {
-        try {
-          if (!req) return res.sendStatus(400);
-          const body = req.body;
+  async getAppointments(req, res, next) {
+    try {
+      if (!req) return res.sendStatus(400);
+      const body = req.body;
+      //генерация расписания
+      let appointments = [];
+      const startHour = 8;
+      const endHour = 20;
+      const duration = 30;
 
-          const user = await prisma.schedule.findMany({
-            where: {
-              is_free: {
-                equals: true,
-              },
-              doctor_id:{
-                equals: body.doctor_id
-              },
-              time_from: {
-                lte: new Date(body.date + " 23:59:59").toISOString(),
-                gte: new Date(body.date).toISOString()
-              }
-            },
+      for (let hour = startHour; hour < endHour; hour++) {
+        for (let minutes = 0; minutes < 60; minutes += duration) {
+          const startTime = new Date(body.date);
+          startTime.setHours(hour, minutes);
+
+          const endTime = new Date(startTime);
+          endTime.setMinutes(startTime.getMinutes() + duration);
+
+          appointments.push({
+            doctor_id: body.doctor_id,
+            time_from: startTime,
+            time_to: endTime,
           });
-          res.json(user);
-        } catch (err) {
-          console.log(err);
-          res.json(err);
         }
       }
+      //получение занятых слотов
+      const busyAppoinments = await prisma.schedule.findMany({
+        select: {
+          doctor_id: true,
+          time_from: true,
+          time_to: true,
+        },
+        where: {
+          is_free: {
+            equals: false,
+          },
+          doctor_id: {
+            equals: body.doctor_id,
+          },
+        },
+      });
+      //поиск и удаление пересечении занятых временных слотах во всех слотах
+      const s = new Set(busyAppoinments.map((e) => JSON.stringify(e)));
+      const freeAppoinments = appointments.filter(
+        (e) => !s.has(JSON.stringify(e))
+      );
+
+      console.log(freeAppoinments);
+      res.json(freeAppoinments);
+    } catch (err) {
+      console.log(err);
+      res.json(err);
+    }
+  }
 }
 
-module.exports = new doctorController()
+module.exports = new doctorController();
