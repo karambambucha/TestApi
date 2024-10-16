481967d8c4812878c3e963949fe4ccaaef4c3332
karambambucha <tsmfdr@yandex.ru>
2024-10-17 00:14:04 +0700

    fix time doctorController

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -11,12 +11,11 @@ class doctorController {
       const startHour = 8;
       const endHour = 20;
       const duration = 30;
-
+      const time = body.date.split("-")
       for (let hour = startHour; hour < endHour; hour++) {
         for (let minutes = 0; minutes < 60; minutes += duration) {
-          const startTime = new Date(body.date);
-          startTime.setHours(hour, minutes);
-
+          const startTime = new Date(Date.UTC(time[0], time[1]-1, time[2], hour, minutes, 0, 0));
+          console.log(startTime)
           const endTime = new Date(startTime);
           endTime.setMinutes(startTime.getMinutes() + duration);
 
@@ -48,8 +47,7 @@ class doctorController {
       const freeAppoinments = appointments.filter(
         (e) => !s.has(JSON.stringify(e))
       );
-
-      console.log(freeAppoinments);
+      
       res.json(freeAppoinments);
     } catch (err) {
       console.log(err);
