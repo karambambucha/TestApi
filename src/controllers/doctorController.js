d253adf66cf647d97ec5a117ccfc1458909b6626
karambambucha <tsmfdr@yandex.ru>
2024-10-17 11:57:20 +0700

    change getAppointments

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -11,16 +11,17 @@ class doctorController {
       const startHour = 8;
       const endHour = 20;
       const duration = 30;
-      const time = body.date.split("-")
+      const time = body.date.split("-");
       for (let hour = startHour; hour < endHour; hour++) {
         for (let minutes = 0; minutes < 60; minutes += duration) {
-          const startTime = new Date(Date.UTC(time[0], time[1]-1, time[2], hour, minutes, 0, 0));
-          console.log(startTime)
+          const startTime = new Date(
+            Date.UTC(time[0], time[1] - 1, time[2], hour, minutes, 0, 0)
+          );
+          //time[1]-1 потому что почему-то выводится на месяц больше: time[1]=10 => startTime.month = 11
           const endTime = new Date(startTime);
           endTime.setMinutes(startTime.getMinutes() + duration);
 
           appointments.push({
-            doctor_id: body.doctor_id,
             time_from: startTime,
             time_to: endTime,
           });
@@ -29,7 +30,6 @@ class doctorController {
       //получение занятых слотов
       const busyAppoinments = await prisma.schedule.findMany({
         select: {
-          doctor_id: true,
           time_from: true,
           time_to: true,
         },
@@ -43,11 +43,12 @@ class doctorController {
         },
       });
       //поиск и удаление пересечении занятых временных слотах во всех слотах
+
       const s = new Set(busyAppoinments.map((e) => JSON.stringify(e)));
       const freeAppoinments = appointments.filter(
         (e) => !s.has(JSON.stringify(e))
       );
-      
+
       res.json(freeAppoinments);
     } catch (err) {
       console.log(err);
