3be35f127a0ca9d3b9e7b8d1746a164f82c17e90
karambambucha <tsmfdr@yandex.ru>
2024-10-17 12:01:26 +0700

    add .env example

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -12,11 +12,13 @@ class doctorController {
       const endHour = 20;
       const duration = 30;
       const time = body.date.split("-");
+
       for (let hour = startHour; hour < endHour; hour++) {
         for (let minutes = 0; minutes < 60; minutes += duration) {
           const startTime = new Date(
             Date.UTC(time[0], time[1] - 1, time[2], hour, minutes, 0, 0)
           );
+
           //time[1]-1 потому что почему-то выводится на месяц больше: time[1]=10 => startTime.month = 11
           const endTime = new Date(startTime);
           endTime.setMinutes(startTime.getMinutes() + duration);
@@ -33,6 +35,7 @@ class doctorController {
           time_from: true,
           time_to: true,
         },
+
         where: {
           is_free: {
             equals: false,
@@ -42,6 +45,7 @@ class doctorController {
           },
         },
       });
+
       //поиск и удаление пересечении занятых временных слотах во всех слотах
 
       const s = new Set(busyAppoinments.map((e) => JSON.stringify(e)));
